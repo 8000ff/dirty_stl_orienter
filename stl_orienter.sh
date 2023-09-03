f01625cb4f5e468242e453306e0da35b0e585db4
8000ff <->
2023-09-03 13:32:10 +0200

    Use ** glob

--- stl_orienter.sh
+++ stl_orienter.sh
@@ -3,6 +3,6 @@ if [ ! -d "$1" ]; then
     echo "usage: $0 inDir outDir"
     exit 1
 fi
-ls ./"$1"/*.stl -1 | xargs -I{} ./center.py {}
-ls ./"$1"/*.stl -1 | xargs -I{} ./param.sh {}
-ls ./"$1"/*.stl -1 | xargs -I{} ./apply.sh {} "$2"
+ls ./"$1"/**/*.stl -1 | xargs -I{} ./center.py {}
+ls ./"$1"/**/*.stl -1 | xargs -I{} ./param.sh {}
+ls ./"$1"/**/*.stl -1 | xargs -I{} ./apply.sh {} "$2"
